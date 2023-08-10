6a243048aa62e5c9157c7478bb6134aa89261de8
lucidrains <lucidrains@gmail.com>
2023-08-10 13:32:19 -0700

    complete the basic idea for soft moe, including the l2norm w/ scale

--- README.md
+++ README.md
@@ -8,6 +8,29 @@ Implementation of <a href="https://arxiv.org/abs/2308.00951">Soft MoE (Mixture o
 
 This MoE has only been made to work with non-autoregressive encoder. However, some recent <a href="https://arxiv.org/abs/2305.18295">text-to-image models</a> have started using MoE with great results, so may be a fit there.
 
+## Install
+
+```bash
+$ pip install soft-moe-pytorch
+```
+
+## Usage
+
+```python
+import torch
+from soft_moe_pytorch import SoftMoE
+
+moe = SoftMoE(
+    dim = 512,         # model dimensions
+    seq_len = 1024,    # max sequence length (will automatically calculate number of slots as seq_len // num_experts) - you can also set num_slots directly
+    num_experts = 4    # number of experts - (they suggest number of experts should be high enough that each of them get only 1 slot. wonder if that is the weakness of the paper?)
+)
+
+x = torch.randn(1, 1024, 512)
+
+out = moe(x) + x # (1, 1024, 512) - add in a transformer in place of a feedforward at a certain layer (here showing the residual too)
+```
+
 ## Citations
 
 ```bibtex
--- soft_moe_pytorch/soft_moe.py
+++ soft_moe_pytorch/soft_moe.py
@@ -5,12 +5,103 @@ from torch import nn, einsum, Tensor
 
 from einops import rearrange
 
+# helper functions
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+def l2norm(t):
+    return F.normalize(t, dim = - 1)
+
+# norm
+
+class RMSNorm(Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.scale = dim ** 0.5
+        self.gamma = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        return l2norm(x) * self.scale * self.gamma
+
+# expert
+
+def Expert(dim, mult = 4, dropout = 0.):
+    dim_hidden = int(dim * mult)
+    return nn.Sequential(
+        nn.Linear(dim, dim_hidden),
+        nn.GELU(),
+        nn.Dropout(dropout),
+        nn.Linear(dim_hidden, dim)
+    )
+
+# main class
+
 class SoftMoE(Module):
     def __init__(
         self,
-        dim
+        dim,
+        *,
+        seq_len = None,
+        num_experts = 4,
+        num_slots = None,
+        expert_mult = 4,
+        dropout = 0.
     ):
         super().__init__()
+        assert exists(seq_len) ^ exists(num_slots), 'either seq_len, or num_slots must be passed into SoftMoE'
+
+        num_slots = default(num_slots, seq_len // num_experts)
+
+        self.norm = RMSNorm(dim)
+
+        self.slot_norm = RMSNorm(dim)
+        self.slot_embeds = nn.Parameter(torch.randn(num_experts, num_slots, dim))
+
+        self.experts = nn.ModuleList([
+            Expert(dim = dim, mult = expert_mult, dropout = dropout) for _ in range(num_experts)
+        ])
 
     def forward(self, x):
-        return x
+        """
+        einstein notation
+        b - batch
+        n - sequence length
+        e - number of experts
+        s - number of slots per expert
+        d - feature dimension
+        """
+
+        # following Algorithm 1, with the normalization they proposed, but with scaling of both (the now popular rmsnorm + gamma)
+
+        x = self.norm(x)
+        slot_embeds = self.slot_norm(self.slot_embeds)
+
+        logits = einsum('b n d, e s d -> b n e s', x, slot_embeds)
+
+        dispatch_weights = logits.softmax(dim = 1)
+
+        combine_weights = rearrange(logits, 'b n e s -> b n (e s)')
+        combine_weights = combine_weights.softmax(dim = -1)
+
+        # derive slots by weighted average of input tokens using the dispatch weights from above
+
+        slots = einsum('b n d, b n e s -> e b s d', x, dispatch_weights)
+
+        # route the slots per expert to each expert
+
+        out = []
+        for slots_per_expert, expert in zip(slots, self.experts):
+            out.append(expert(slots_per_expert))
+
+        out = torch.stack(out)
+
+        # combine back out
+
+        out = rearrange(out, 'e b s d -> b (e s) d')
+        out = einsum('b s d, b n s -> b n d', out, combine_weights)
+
+        return out
