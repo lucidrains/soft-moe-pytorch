a505e509c745270f4996aa2a317856bd7c69abe1
lucidrains <lucidrains@gmail.com>
2023-08-11 14:37:59 -0700

    0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'soft-moe-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.2',
+  version = '0.0.3',
   license='MIT',
   description = 'Soft MoE - Pytorch',
   author = 'Phil Wang',
