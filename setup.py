8c3fedbb92e9c98ed6bd6e80a797fa4c2f14b32c
lucidrains <lucidrains@gmail.com>
2023-12-14 12:03:27 -0800

    add a contiguous, thanks to @conceptofmind

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'soft-moe-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.1.0',
+  version = '0.1.1',
   license='MIT',
   description = 'Soft MoE - Pytorch',
   author = 'Phil Wang',
